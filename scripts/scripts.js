9e58e88397ed86e41adf07436e6ee7ae9094db4a
John Anthony <jmanthony.fullsail@gmail.com>
2021-09-14 23:44:02 -0500

    finished the function for start and end dates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,8 @@
 console.log('this is JS');
 
+const sTime = new Date(2021, 8, 14);
+const eTime = new Date(2021, 8, 15);
+
 //  incantation setup
 //  require() takes a path to a file
 let location = require('./locations.js');
@@ -7,3 +10,24 @@ let location = require('./locations.js');
 for (let locations of location) {
   console.log(`Your appointment will be scheduled in the ${locations} area.`);
 }
+
+//  time for the scheduling
+//  function for the time between jobs
+function time(endTime, startTime) {
+  /*
+       using the js date formatting methods to extract the time between
+       the last appointment and the upcoming appointment
+  */
+
+  const yearDifference = endTime.getFullYear() - startTime.getFullYear();
+  const monthDifference = endTime.getMonth() - startTime.getMonth();
+  const dateDifference = endTime.getDate() - startTime.getDate();
+  const hourDifference = endTime.getHours() - startTime.getHours();
+  const minuteDifference = endTime.getMinutes() - startTime.getMinutes();
+  const secondDifference = endTime.getSeconds() - startTime.getSeconds();
+
+  return `You have ${yearDifference} year, ${monthDifference} months, ${dateDifference} days, ${hourDifference} hours, ${minuteDifference} minutes, and ${secondDifference} seconds`;
+}
+
+let timeDifference = time(eTime, sTime);
+console.log(timeDifference, `until the next available appointment.`);
