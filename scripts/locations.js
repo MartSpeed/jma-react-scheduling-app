475fe74c5f12c6fc058b7219807a7dad26ae9569
John Anthony <jmanthony.fullsail@gmail.com>
2021-09-14 19:28:27 -0500

    node test and folder routes ops check good

---
+++ scripts/locations.js
@@ -0,0 +1,11 @@
+let locations = [
+  'San Diego',
+  'Oceanside',
+  'El Cajon',
+  'San Diego',
+  'La Mesa',
+  'El Cajon',
+];
+
+//  data needs to exported
+module.exports = locations;
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,7 @@
 console.log('this is JS');
+
+//  incantation setup
+//  require() takes a path to a file
+let location = require('./locations.js');
+
+console.log(location);
